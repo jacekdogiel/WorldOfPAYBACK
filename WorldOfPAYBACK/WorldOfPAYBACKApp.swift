284a26de221ad491497cda1429404eae23b0d8ba
Jacek <jacek.dogiel@gmail.com>
2023-10-11 14:32:18 +0200

    Initial Commit

---
+++ WorldOfPAYBACK/WorldOfPAYBACKApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldOfPAYBACKApp.swift
+//  WorldOfPAYBACK
+//
+//  Created by Jacek Dogiel on 11/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct WorldOfPAYBACKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
